79aa754672517d43132586e08ef23fa2eb0bc1fe
wogur6594-dotcom <wogur6594@gmail.com>
2026-04-16 15:36:12 +0900

    init

---
+++ src/main/java/com/jh/app/Kosmo163PostApplication.java
@@ -0,0 +1,13 @@
+package com.jh.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kosmo163PostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kosmo163PostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jh/app/Kosmo163PostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jh.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kosmo163PostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
